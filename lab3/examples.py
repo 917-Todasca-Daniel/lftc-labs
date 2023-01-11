9d3c49a01179dcb029859bac95a0c782fe491e72
Quackdratic <39701964+Quackdratic@users.noreply.github.com>
2023-01-11 14:10:27 +0200

    lab8 lex with PFI

--- lab3/examples.py
+++ lab3/examples.py
@@ -7,4 +7,4 @@ from Scanner import Scanner
 # Scanner().scan_file("C:/Users/quackdratic/Desktop/lftc/lftc-labs/lab1a/p1.txt")
 Scanner().scan_file("C:/Users/quackdratic/Desktop/lftc/lftc-labs/lab1a/p2.txt")
 # Scanner().scan_file("C:/Users/quackdratic/Desktop/lftc/lftc-labs/lab1a/p3.txt")
-# Scanner().scan_file("C:/Users/quackdratic/Desktop/lftc/lftc-labs/lab1a/p1err.txt")
+Scanner().scan_file("C:/Users/quackdratic/Desktop/lftc/lftc-labs/lab1a/p1err.txt")
